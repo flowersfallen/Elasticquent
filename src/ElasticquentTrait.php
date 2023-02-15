24146580542dd6805aae963705f7f054d442576e
onward-web <developer@onward-web.com>
2023-02-15 15:14:03 +0200

    update

--- src/ElasticquentTrait.php
+++ src/ElasticquentTrait.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Collection;
 trait ElasticquentTrait
 {
     use ElasticquentClientTrait;
+    use ElasticquentBaseTrait;
 
     /**
      * Uses Timestamps In Index
@@ -92,36 +93,7 @@ trait ElasticquentTrait
         $this->useTimestampsInIndex(false);
     }
 
-    /**
-     * Get Mapping Properties
-     *
-     * @return array
-     */
-    public function getMappingProperties()
-    {
-        return $this->mappingProperties;
-    }
-
-    /**
-     * Set Mapping Properties
-     *
-     * @param    array $mapping
-     * @internal param array $mapping
-     */
-    public function setMappingProperties(array $mapping = null)
-    {
-        $this->mappingProperties = $mapping;
-    }
-
-    /**
-     * Get Index Settings
-     *
-     * @return array
-     */
-    public function getIndexSettings()
-    {
-        return $this->indexSettings;
-    }
+   
 
     /**
      * Is Elasticsearch Document
@@ -445,158 +417,13 @@ trait ElasticquentTrait
         return $fieldsParam;
     }
 
-    /**
-     * Mapping Exists
-     *
-     * @return bool
-     */
-    public static function mappingExists()
-    {
-        $instance = new static;
-
-        $mapping = $instance->getMapping();
-
-        return (empty($mapping)) ? false : true;
-    }
-
-    /**
-     * Get Mapping
-     *
-     * @return void
-     */
-    public static function getMapping()
-    {
-        $instance = new static;
-
-        $params = $instance->getBasicEsParams();
-
-        return $instance->getElasticSearchClient()->indices()->getMapping($params);
-    }
-
-    /**
-     * Put Mapping.
-     *
-     * @param bool $ignoreConflicts
-     *
-     * @return array
-     */
-    public static function putMapping($ignoreConflicts = false)
-    {
-        $instance = new static;
-
-        $mapping = $instance->getBasicEsParams();
-
-        $params = array(
-            '_source' => array('enabled' => true),
-            'properties' => $instance->getMappingProperties(),
-        );
-
-        $mapping['body'] = $params;
-
-        return $instance->getElasticSearchClient()->indices()->putMapping($mapping);
-    }
-
-    /**
-     * Create Index
-     *
-     * @param int $shards
-     * @param int $replicas
-     *
-     * @return array
-     */
-    public static function createIndex($addMapping = true, $shards = null, $replicas = null)
-    {
-        $instance = new static;
-
-        $client = $instance->getElasticSearchClient();
-
-        $index = array(
-            'index' => $instance->getIndexName(),
-        );
-
-        $settings = $instance->getIndexSettings();
-        if (!is_null($settings)) {
-            $index['body']['settings'] = $settings;
-        }
-
-        if (!is_null($shards)) {
-            $index['body']['settings']['number_of_shards'] = $shards;
-        }
-
-        if (!is_null($replicas)) {
-            $index['body']['settings']['number_of_replicas'] = $replicas;
-        }
-
-        if($addMapping){
-            $mappingProperties = $instance->getMappingProperties();
-            if (!is_null($mappingProperties)) {
-                $index['body']['mappings'] = [
-                    '_source' => array('enabled' => true),
-                    'properties' => $mappingProperties,
-                ];
-            }
-        }
-
-
-        return $client->indices()->create($index);
-    }
-
-    /**
-     * Delete Index
-     *
-     * @return array
-     */
-    public static function deleteIndex()
-    {
-        $instance = new static;
-
-        $client = $instance->getElasticSearchClient();
-
-        $index = array(
-            'index' => $instance->getIndexName(),
-        );
-
-        return $client->indices()->delete($index);
-    }
-
-
-    /**
-     * Index exist
-     *
-     * @return bool
-     */
-    public static function indexExist()
-    {
-        $instance = new static;
-
-        $client = $instance->getElasticSearchClient();
-
-        $index = array(
-            'index' => $instance->getIndexName(),
-        );
-
-        return $client->indices()->exists($index);
-    }
 
 
 
-    public static function putSettings(){
 
-        $instance = new static;
 
-        $client = $instance->getElasticSearchClient();
 
-        $index = array(
-            'index' => $instance->getIndexName(),
-        );
 
-        $settings = $instance->getIndexSettings();
-        if (!is_null($settings)) {
-            $index['body']['settings'] = $settings;
-        }
-
-        $client->indices()->putSettings($index);
-    }
 
     /**
      * New From Hit Builder
